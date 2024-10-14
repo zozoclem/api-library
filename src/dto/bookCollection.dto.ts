9cdf84e362c506703fa9c35c2a4a6a1f4d1508f4
jdelcuserobert <jdelcuserobert@okteo.fr>
2024-10-14 22:01:52 +0200

    feat: Correction question 4.2

--- src/dto/bookCollection.dto.ts
+++ src/dto/bookCollection.dto.ts
@@ -1,1 +1,55 @@
-export interface BookCollectionDTO {}
+import { BookDTO } from "./book.dto";
+
+export enum State {
+  INUTILISABLE = "Inutilisable",
+  MAUVAIS = "Mauvais",
+  PASSABLE = "Passable",
+  BON = "Bon",
+  TRES_BON = "Très bon",
+  NEUF = "Neuf",
+}
+
+export function getStateFromInteger(value: number): State {
+  switch (value) {
+    case 0:
+      return State.INUTILISABLE;
+    case 1:
+      return State.MAUVAIS;
+    case 2:
+      return State.PASSABLE;
+    case 3:
+      return State.BON;
+    case 4:
+      return State.TRES_BON;
+    case 5:
+      return State.NEUF;
+    default:
+      const error = new Error("State out of range");
+      (error as any).status = 412;
+      throw error;
+  }
+}
+
+export function getIntegerFromState(value: State): number {
+  switch (value) {
+    case State.INUTILISABLE:
+      return 0;
+    case State.MAUVAIS:
+      return 1;
+    case State.PASSABLE:
+      return 2;
+    case State.BON:
+      return 3;
+    case State.TRES_BON:
+      return 4;
+    case State.NEUF:
+      return 5;
+  }
+}
+
+export interface BookCollectionDTO {
+  id: number;
+  book: BookDTO;
+  available: number;
+  state: State;
+}
